ccb6abb6d7f64e5d6d42f3e81953f32da58f4c88
mickey-40 <mickeyarnold53@yahoo.com>
2023-12-26 13:20:21 -0600

    finished script.js and completed app

--- script.js
+++ script.js
@@ -1,93 +1,93 @@
-//create a quiz class
+// CREATE A QUIZ CLASS
 class Quiz {
-  constructor(questions){
-    this.score = 0;
-    this.questions = questions;
-    this.questionsIndex = 0;
+  constructor(questions) {
+      this.score = 0;
+      this.questions = questions;
+      this.questionIndex = 0;
   }
 
-  getQuestionIndex(){
-    return this.questions[this.questionIndex];
-  }
-  
-  guess(answer){
-    if(this.getQuestionIndex().isCorrectAnswer(answer)){
-      this.score++;
-    }
-    this.getQuestionIndex++;
+  getQuestionIndex() {
+      return this.questions[this.questionIndex];
   }
 
-  isEnded(){
-    return this.questionsIndex === this.questions.length;
+  guess(answer) {
+      if (this.getQuestionIndex().isCorrectAnswer(answer)) {
+          this.score++;
+      }
+      this.questionIndex++;
+  }
+
+  isEnded() {
+      return this.questionIndex === this.questions.length;
   }
 }
 
-//Create a question class
+// Create a question Class
 class Question {
-  constructor(text, choices, answer){
-    this.text = text;
-    this.choices = choices;
-    this.answer = answer;
+  constructor(text, choices, answer) {
+      this.text = text;
+      this.choices = choices;
+      this.answer = answer;
   }
 
-  isCorrectAnswer(choice){
-    return this.answer === choice;
+  isCorrectAnswer(choice) {
+      return this.answer === choice;
   }
 }
 
-// display question
-function displayQuestion(){
+// NOW DISPLAY THE QUESTIONS
+function displayQuestion() {
   if (quiz.isEnded()) {
-    showScores();
+      showScores();
   } else {
-    //show question
-    let questionElement = document.getElementById("question");
-    questionElement.innerHTML = quiz.getQuestionIndex().text;
+      // show question
+      let questionElement = document.getElementById("question");
+      questionElement.innerHTML = quiz.getQuestionIndex().text;
 
-    // show options
-    let choices = quiz.getQuestionIndex().choices;
-    for (let i = 0; i < choices.length; i++) {
-      let choiceElement = document.getElementById("choice" + i);
-      choiceElement.innerHTML = choices[i];
-      guess("btn" + i, choices[i]);
-    }
+      // show options
+      let choices = quiz.getQuestionIndex().choices;
+      for (let i = 0; i < choices.length; i++) {
+          let choiceElement = document.getElementById("choice" + i);
+          choiceElement.innerHTML = choices[i];
+          guess("btn" + i, choices[i]);
+      }
 
-    showProgress();
+      showProgress();
   }
 };
 
-// Guess function
-function guess(id, guess){
+// GUESS ANSWER
+function guess(id, guess) {
   let button = document.getElementById(id);
   button.onclick = function() {
-    quiz.guess(guess);
-    displayQuestion();
+      quiz.guess(guess);
+      displayQuestion();
   }
 };
 
-//show quiz progess
+// SHOW QUIZ PROGRESS
 function showProgress() {
   let currentQuestionNumber = quiz.questionIndex + 1;
-  let progessElement = document.getElementById("progress");
-  progessElement.innerHTML = 
-  `Question ${currentQuestionNumber} of ${quiz.questions.length}`;
-}
+  let ProgressElement = document.getElementById("progress");
+  ProgressElement.innerHTML =
+      `Question ${currentQuestionNumber} of ${quiz.questions.length}`;
+};
 
-// show score
-function showScores () {
-  let quizEndHtml =
-  `
-    <h1>Quiz Completed</h1>
-    <h2 id="score">You scored: ${quiz.score} of ${quiz.questions.length}</h2>
-    <div class="quiz-repeat">
+// SHOW SCORES
+function showScores() {
+  let quizEndHTML =
+      `
+  <h1>Quiz Completed</h1>
+  <h2 id='score'> Your scored: ${quiz.score} of ${quiz.questions.length}</h2>
+  <div class="quiz-repeat">
       <a href="index.html">Take Quiz Again</a>
-    </div>
+  </div>
   `;
   let quizElement = document.getElementById("quiz");
-  quizElement.innerHTML = quizEndHtml;
-}
+  quizElement.innerHTML = quizEndHTML;
+};
 
-// create quiz questions
+// create questions here
 let questions = [
   new Question(
       "Hyper Text Markup Language Stands For?", ["JQuery", "XHTML", "CSS", "HTML"], "HTML"
@@ -106,7 +106,31 @@ let questions = [
   )
 ];
 
+// INITIALIZE quiz
 let quiz = new Quiz(questions);
 
-//display quesiton
+// display questions
 displayQuestion();
+
+// add a countdown
+let time = 10;
+let quizTimeInMinutes = time * 60 * 60;
+quizTime = quizTimeInMinutes / 60; 
+
+let counting = document.getElementById("count-down");
+
+function startCountdown() {
+  let quizTimer = setInterval(function(){
+    if (quizTime <= 0) {
+      clearInterval(quizTimer);
+      showScores();
+    } else {
+      quizTime--;
+      let sec = Math.floor(quizTime % 60);
+      let min = Math.floor(quizTime / 60 ) % 60;
+      counting.innerHTML = `Time: ${min} : ${sec}`
+    }
+  }, 1000) 
+}
+
+startCountdown();
